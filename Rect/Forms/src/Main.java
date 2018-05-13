ebe87c343dcfc573d133fc82c37026912447948f
NicolasFrancaX <nicolasfranca9@gmail.com>
2018-05-13 01:58:17 -0300

    Adicionando arquivos

---
+++ Rect/Forms/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Rect retangulo = new Rect(10, 10);
+		
+		System.out.println(retangulo.area());
+		System.out.println(retangulo.perimeter());
+		System.out.println(retangulo.diagonal());
+	}
+
+}
---
+++ bla.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Bla\n");
+    return 0;
+}
